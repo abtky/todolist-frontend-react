86a1c10faa9e245f79891bf92291cde7dadcd050
takuya.abe <takuya.abe@linecorp.com>
2022-09-19 14:37:38 +0900

    feat: Add input form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useTasks } from "./hooks/useTasks";
 
-function App() {
+const App: React.FC = () => {
+  const { tasks, addTask } = useTasks();
+  const [inputValue, setInputValue] = useState('');
+  const handleAddClick = () => {
+    addTask(inputValue);
+    setInputValue('');
+  }
+  const myTasks = tasks.join()
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>task: {myTasks}</p>
+        <input type="text" value={inputValue} onChange={(e) => { setInputValue(e.target.value) }} />
+        <input type="button" value="add" onClick={handleAddClick} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -21,6 +32,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
