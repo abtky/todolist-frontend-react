5ca6b39a2a8e8c43bd771eb4d3b649bd26b2cd0e
takuya.abe <takuya.abe@linecorp.com>
2022-09-21 08:56:16 +0900

    refactor: Split component

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,16 @@ import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { useTasks } from "./hooks/useTasks";
+import { TaskList } from "./components/TaskList";
 
 const App: React.FC = () => {
   const { tasks, addTask, removeTask } = useTasks();
-  const [inputValue, setInputValue] = useState('');
-  const handleAddClick = () => {
-    addTask(inputValue);
-    setInputValue('');
-  }
-  const handleDeleteClick = (taskId: number) => {
-    console.log('handleDeleteClick', taskId);
-    removeTask(taskId);
-  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {tasks.map((task) => (<p key={task.id}>{task.label}<button onClick={() => { handleDeleteClick(task.id) }}>Delete</button></p>))}
-        <input type="text" value={inputValue} onChange={(e) => { setInputValue(e.target.value) }} />
-        <input type="button" value="add" onClick={handleAddClick} />
       </header>
+      <TaskList tasks={tasks} addTask={addTask} removeTask={removeTask} />
     </div>
   );
 };
