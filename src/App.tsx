b11cd7d0e1494116e5e0dc06ea3741f87dcc8d7a
takuya.abe <takuya.abe@linecorp.com>
2022-09-27 23:28:33 +0900

    refactor: Resolve eslint problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-/* eslint-disable @typescript-eslint/no-misused-promises */
-/* eslint-disable react/jsx-filename-extension */
-/* eslint-disable import/extensions */
-/* eslint-disable import/no-unresolved */
 import React from 'react';
 import styled from '@emotion/styled';
 import { keyframes } from '@emotion/react';
@@ -44,12 +40,22 @@ const AppLogo = styled.img`
 
 const App: React.FC = () => {
   const { tasks, addTask, removeTask } = useTasks();
+  const addTaskRef = (label: string): void => {
+    addTask(label).catch((e) => {
+      throw e;
+    });
+  };
+  const removeTaskRef = (id: number): void => {
+    removeTask(id).catch((e) => {
+      throw e;
+    });
+  };
   return (
     <AppRoot>
       <AppHeader>
         <AppLogo src={logo} alt="logo" />
       </AppHeader>
-      <TaskList tasks={tasks} addTask={addTask} removeTask={removeTask} />
+      <TaskList tasks={tasks} addTask={addTaskRef} removeTask={removeTaskRef} />
     </AppRoot>
   );
 };
